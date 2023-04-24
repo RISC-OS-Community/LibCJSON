fba136d273ea6ba5de6ee0201683f309822bb85f
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2023-04-24 03:12:06 +0100

    found an issue with codeQL, it needs manual path to get into ux-src

--- src/MkGCC.sh
+++ src/MkGCC.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-make static OS=`uname -s` -f MakeFileGCC
+current_dir="$1"
+if [ "$current_dir" == "" ]; then
+    current_dir="$(pwd)"
+fi
+cd ${current_dir}
+make static OS=`uname -s` -f ./MakeFileGCC
