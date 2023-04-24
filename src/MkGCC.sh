435c52557e9204b33e48ceedb5d3d7213e301f41
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2023-04-24 03:21:15 +0100

    another test to check if codeQL is finally happy with RISC OS source tree

--- src/MkGCC.sh
+++ src/MkGCC.sh
@@ -4,4 +4,9 @@ if [ "$current_dir" == "" ]; then
     current_dir="$(pwd)"
 fi
 cd ${current_dir}
+
+# Display MakefielGCC
+cat ${current_dir}/MakeFileGCC
+
+# Make the artifacts
 make static OS=`uname -s` -f ${current_dir}/MakeFileGCC
